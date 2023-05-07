e50b365209456fbb640fbc3f4baa4aa45e3f95b6
Резанович Игорь <Rezanovich@jukola.office>
2023-05-07 18:31:10 +0300

    feat: implement 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const fsp = fs.promises;
+const path = require("path");
+
+async function mergeStyles(src, dest) {
+  const output = fs.createWriteStream(dest);
+
+  const files = await fsp.readdir(src);
+  for (const file of files) {
+    fileName = path.resolve(src, file);
+    const stat = await fsp.stat(fileName);
+    if (!stat.isFile() || path.extname(file) !== ".css") {
+      continue;
+    }
+
+    fs.createReadStream(fileName).pipe(output);
+  }
+}
+
+mergeStyles(
+  path.resolve(__dirname, "styles"),
+  path.resolve(__dirname, "project-dist/bundle.css")
+);
